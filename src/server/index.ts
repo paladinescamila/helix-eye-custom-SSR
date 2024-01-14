85d5e9c63b0b31252f2d74a4b80b3dd14283edb3
Camila Paladines <camilapaladines27@gmail.com>
2024-01-13 22:14:51 -0500

    Add html dinamically

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,11 @@
 import express, {Express, Request, Response} from 'express';
 import {config} from './config';
+import {template} from './render/template';
 
 const app: Express = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
+app.get('*', (req: Request, res: Response) => {
+    res.send(template(`<h1>Hello World</h1>`));
 });
 
 app.listen(config.PORT, () => {
